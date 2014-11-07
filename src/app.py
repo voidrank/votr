339db3ebb34dccac84aa585adc0610ff5655ffa0
voidrank <452727698@qq.com>
2014-11-08 00:31:18 +0800

    add-login&homepage's-foreground(python part)

--- src/app.py
+++ src/app.py
@@ -4,6 +4,8 @@ import json
 import pymongo
 import urllib
 
+import models
+
 #initial
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -12,10 +14,33 @@ app.secret_key = os.urandom(24)
 conn = pymongo.Connection()
 db = conn.votr
 
-@app.route('/', methods=["GET"])
-def index():
-	items = db.item.find().sort([("post-time",-1)])
-	return render_template("index.html", items=items)
+@app.route("/", methods["GET", "POST"])
+def login():
+	if request.method == "GET":
+		return render_template("login.html", session=session)
+	else:
+		uid = request.form["uid"]
+		passwd = request.form["passwd"]
+		if models.user.login(uid=uid,passwd=passwd) == "success":
+			session["uid"] = uid
+			signal = "Success"
+		else:
+			signal = "Wrong uid or passwd"
+		return render_template("login.html", session=session, signal=signal)
+
+@app.route("/homepage/", methods=["GET"]):
+def homepage():
+	if models.user.exist(session["uid"]):
+		hostItem = models.user.hostItem.get(session["uid"],limit=20)
+		partiItem = models.user.partiItem.get(session["uid"],limit=20)
+		return render_template("homepage.html", hostItem=hostItem, partiItem=partiItem)
+	else:
+		abort(404);
+
+
+@app.route('/<hostName>/<itemName>', methods=["GET"])
+def item2(hostName, itemName):
+	return "1"
 
 @app.route("/<item_name>", methods=["GET", "POST"])
 def item(item_name):
